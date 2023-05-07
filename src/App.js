b4f4d5cce7fd343fd4ed50528faa0e2dc11bef2e
Bhargav <bhargavbalu63@gmail.com>
2023-05-07 12:26:23 +0530

    todo front end added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 
 function App() {
+const [task, setTask]=useState([])
+
+const [newtask, setNewtask]= useState("")
+
+
+useEffect(
+  ()=>
+  {
+
+   axios.get("http://localhost:5000/").then(response=>setTask(response.data));   
+      
+  },[])
+
+ const submitHandler= ()=>
+    {
+       axios.post("http://localhost:5000/addtask", {todo:newtask}).then(response=> setTask(response.data))
+       
+    }
+const deleteHandler=(id)=>
+{
+
+  axios.delete(`http://localhost:5000/delete/${id}`).then(response=> setTask(response.data))
+
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <h1>Todo - app</h1>
+   <form onSubmit={submitHandler}>
+   <input placeholder='add your task' id="input" type="text" value={newtask} onChange={(e)=>setNewtask(e.target.value)} />
+      <input id="submit" value="Add" type="submit"/>
+   </form>
+      
+
+   {/* {task.map(item => (
+     <div>
+      {item.todo}
+      </div>
+   ))} */}
+
+   
+   {task.map((each)=>
+   {
+
+    return(
+      <div key={each._id} id="task">
+
+      <li >
+
+         {each.todo}
+          
+        </li>
+        <button id="delete" onClick={()=>deleteHandler(each._id)}>delete</button>
+        </div>
+    )
+
+
+   })}
+   
+   
+   
+
+
+
+
     </div>
   );
 }
